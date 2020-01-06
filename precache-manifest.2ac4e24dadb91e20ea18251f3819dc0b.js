d7ca7c3c33f0415bfe176259c7e37cd5e4eafa88
Bruno Larouche <bruno.larouche@gmail.com>
2020-01-05 22:52:56 -0500

    Updates

---
+++ precache-manifest.2ac4e24dadb91e20ea18251f3819dc0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c352ab6f15139a27ef802ae9e1b15a0",
+    "url": "/war-room-roller/index.html"
+  },
+  {
+    "revision": "23c5cd795c6200331f07",
+    "url": "/war-room-roller/static/css/main.08a89982.chunk.css"
+  },
+  {
+    "revision": "221cfcbfafa8f8e5c3a8",
+    "url": "/war-room-roller/static/js/2.d2d98275.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/war-room-roller/static/js/2.d2d98275.chunk.js.LICENSE"
+  },
+  {
+    "revision": "23c5cd795c6200331f07",
+    "url": "/war-room-roller/static/js/main.3a5c36f5.chunk.js"
+  },
+  {
+    "revision": "b0e5e6496f05e97c881e",
+    "url": "/war-room-roller/static/js/runtime-main.cbc3da42.js"
+  }
+]);
